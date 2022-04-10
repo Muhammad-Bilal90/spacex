5840072ba1f235486fd4c4e1fe7b380cc2883e15
Muhammad Bilal <bkafridi9833@gmail.com>
2022-04-10 08:01:51 +0500

    Update serviceRegister.ts

--- src/Services/serviceRegister.ts
+++ src/Services/serviceRegister.ts
@@ -2,6 +2,6 @@
 export default function swDev() {
 
     let swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`
-    navigator.serviceWorker.register(swUrl).then((result) => console.log(result))
+    navigator.serviceWorker.register(swUrl).then((result) => console.log(result)).catch((error) => console.log('Error: ', error))
   
 }
