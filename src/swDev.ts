6b97115207fa0fa31e166a6475d7d486f7794424
Muhammad Bilal <bkafridi9833@gmail.com>
2022-04-10 07:19:06 +0500

    Update swDev.ts

--- src/swDev.ts
+++ src/swDev.ts
@@ -1,5 +1,5 @@
 export default function swDev(){
-    let swDev = `${process.env.PUBLIC_URL}/sw.ts`
+    let swDev = `${process.env.PUBLIC_URL}/sw.js`
     navigator.serviceWorker.register(swDev).then((result) => {
         console.log('Result', result);
     })
